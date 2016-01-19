5c50c2261bbb40766773101b6e1ab926176fca52
Bo Huang <rorschachhb@gmail.com>
2016-01-19 17:16:00 +0800

    fix isatap.sh

--- isatap.sh
+++ isatap.sh
@@ -1,4 +1,4 @@
-local_ip=`/sbin/ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "{地址,addr}:" `
+local_ip=`ifconfig -a|grep inet|grep -v 127.0.0.1|grep -v inet6|awk '{print $2}'|tr -d "{地址,addr}:" `
 ip tunnel add sit1 mode sit remote 59.66.4.50 local $local_ip
 ifconfig sit1 up
 ifconfig sit1 add 2001:da8:200:900e:0:5efe:$local_ip/64
