56a40d555d8f5a32c7bad09b88036a6a50cd0bbe
Bo Huang <rorschachhb@gmail.com>
2016-03-01 17:30:11 +0000

    fix automail.sh, write temp files to /tmp

--- automail.sh
+++ automail.sh
@@ -2,14 +2,14 @@
 
 current_ip=`ifconfig | grep 'inet addr:' | grep -v '127.0.0.1' | cut -d : -f2 | awk '{print $1}'`
 
-if [ -f ~/tools/ip.txt ]; then
-    old_ip=`cat ~/tools/ip.txt`
+if [ -f /tmp/ip.txt ]; then
+    old_ip=`cat /tmp/ip.txt`
     if [ "${old_ip}" = "${current_ip}" ]; then
         exit
     fi
 fi
 
 # mail me
-echo "$current_ip `date "+%Y-%m-%d %H:%M:%S"`" | mail -v -s "`hostname` ip update" huangb14@mails.tsinghua.edu.cn 1>>~/tools/mail.log 2>&1
-echo $current_ip > ~/tools/ip.txt
+echo "$current_ip `date "+%Y-%m-%d %H:%M:%S"`" | mail -v -s "`hostname` ip update" huangb14@mails.tsinghua.edu.cn 1>>/tmp/mail.log 2>&1
+echo $current_ip > /tmp/ip.txt
 
